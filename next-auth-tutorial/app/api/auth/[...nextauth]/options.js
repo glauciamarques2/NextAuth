47a6a28fe744251b41713374897598fd96d3ce6d
glauciamarques2 <glauciamarx@gmail.com>
2023-11-15 20:17:29 +0100

    providers and others things of options.js

--- next-auth-tutorial/app/api/auth/[...nextauth]/options.js
+++ next-auth-tutorial/app/api/auth/[...nextauth]/options.js
@@ -2,13 +2,13 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 export const options = {
-    provider: [
+    providers: [
         GithubProvider({
             profile(profile) {
-                console.log("Profile Github", profile);
-
+                console.log("Profile Github: ", profile);
                 let userRole = "Github User";
-                if (profile?.email == "glauciamarx@gmail.com") {                    
+                if (profile?.email == "glauciamarx@gmail.com") {
+                    userRole = "admin";                    
                 }
 
                 return {
@@ -17,12 +17,12 @@ export const options = {
 
                 };
             },
-            clientID: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_Secret,        
+            clientId: process.env.GitHub_ID,
+            clientSecret: process.env.GitHub_Secret,        
         }),
         GoogleProvider({
             profile(profile) {
-                console.log("Profile Google", profile);
+                console.log("Profile Google: ", profile);
 
                 return {
                     ...profile,
@@ -30,7 +30,7 @@ export const options = {
                     role: userRole,
                 };
             },
-            clientID: process.env.GOOGLE_ID,
+            clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_Secret,          
         }),
     ],
